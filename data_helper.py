8c43ee3b28ceb589d054ed651d7b4fbaadf2a796
AgafiaB <bowdenaa@jacks.sfasu.edu>
2025-07-07 15:08:03 -0500

    changed the dataset from sql to directly accessing the files for simplification and ease of access

--- data_helper.py
+++ data_helper.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from torchvision.io import decode_image
-from torch.utils.data import Dataset 
 import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+from torchvision.io import decode_image
+from torch.utils.data import Dataset
 import torch
 import logging
 import numpy as np
@@ -16,112 +17,39 @@ import skimage
 from tqdm import tqdm
 from itertools import combinations
 
-# create a Dataset class to retrieve the data
+
 class SQLDataset_Humanitarian(Dataset):
-    def __init__(self, conn, label_col, img_col='image_path', data_dir=Path(os.path.expanduser('~'), 'CrisisMMD_v2.0','CrisisMMD_v2.0'), 
-                 transform=None, target_transform=None, is_train=False, is_test=False, is_val=False, table_name='Images', 
-                 label_dict = {'other_relevant_information': 1, 'affected_individuals': 2, 'infrastructure_and_utility_damage': 3, 'injured_or_dead_people': 4, 
-                               'rescue_volunteering_or_donation_effort': 5, 'not_humanitarian': 0}):
-        '''
-        Parameters: 
-            conn - a mysql.connector object that will be used to retrieve a cursor 
-            label_col - a name of type string that matches the column name in the sql database that labels the image data
-            img_col - a name of type string that matches the column name in the sql database that contains the image path
-            data_dir - the path that contains the folder containing the image paths in the sql database 
-            transform - pytorch image transformations that transform the data
-            target_transform - does nothing as of now, so do not specify this
-            is_train | is_val | is_test - choose none or one of these; if none chosen, all data is used 
-            table_name - a string that matches the table name; default: "Images"
-            label_dict - a dictionary that maps the labels in the database to integers; default: {'other_relevant_information': 1, 'affected_individuals': 2, 
-                'infrastructure_and_utility_damage': 3, 'injured_or_dead_people': 4,'rescue_volunteering_or_donation_effort': 5, 'not_humanitarian': 0}
-        Notes:
-            is_train uses 90% of the data
-            is_val and is_test each use 5% of the data 
-        '''
-        assert(not ((is_train and is_test) or (is_train and is_val) or (is_val and is_test)), 'a dataset can only be one of either train, test, or val')
-
-        self.conn = conn
-        self.img_col = img_col
-        self.label_col = label_col
+    '''
+    Parameters: 
+    - dir: The directory where the folder containing the images is located
+    - file_path: The path to the TSV file containing the image paths and labels
+    - transform: Optional transform to be applied on a sample (e.g., for data augmentation)
+    '''
+    def __init__(self, dir, file_path, transform=None):
+        self.dir = Path(dir)
+        self.file_path = file_path
+        self.data = pd.read_csv(file_path, sep='\t')
+        self.image_paths = self.data['image'].tolist()
+        self.labels = self.data['label_image']
+        self.label_dict = {value: key for key, value in 
+                           self.data['label_image'].value_counts().reset_index()['label_image'].to_dict().items()}
         self.transform = transform
-        self.target_transform = target_transform
-        self.data_dir = data_dir
-        self.is_train = is_train
-        self.is_val = is_val
-        self.is_test = is_test
-        self.table_name = table_name
-        self.label_dict = label_dict
-
-        cursor = self.conn.cursor()
-
-        # we need a list of available indices 
-        # what idxs are available to use for this database? - depends on the dataset type
-        query = f'SELECT COUNT(image_id) FROM {self.table_name}'
-
-        if not (self.is_train or self.is_val or self.is_test): # if no dataset type specified
-            cursor.execute(query)
-            count = cursor.fetchone()[0]
-            self.possible_sql_idxs = range(count)
-        else:
-            cursor.execute(query)
-            count = cursor.fetchone()[0]
-            
-
-            # below, we use +1 because SQL indexing starts at 1
-            if is_train: 
-                self.possible_sql_idxs = [i+1 for i in range(count) if (((i+1) % 20) < 18)]
-            elif is_val:
-                self.possible_sql_idxs = [i+1 for i in range(count) if (((i+1) % 20) == 18)]
-            else: # must be test
-                self.possible_sql_idxs = [i+1 for i in range(count) if (((i+1) % 20) > 18)]
-        cursor.close()
-
     def __len__(self):
-        '''
-        Returns the number of images in the database 
-        '''
-        return len(self.possible_sql_idxs)
-    
-    # TODO: change this so that it uses the humanitarian classes you are using
+        return len(self.data)
     
     def __getitem__(self, idx):
         '''
-        Description: 
-            Retrieves a tuple of (torch.tensor, string) where the first object is a 3D tensor of image data and the string is the label
+        Returns a tuple of (image, label) for the given index.
+        The image is loaded as a PIL Image, and the label is converted to an integer using the label_dict.
+        If a transform was provided, it is applied to the image.
         '''
-        # retrieve an image from the sql database
-        # return SyntaxError('You need to change this to use humanitarian classes and label column')
-
-        cursor = self.conn.cursor()
-
-        try:
-                query = f'SELECT {self.img_col}, {self.label_col} FROM {self.table_name} WHERE idx={self.possible_sql_idxs[idx]}' 
-                cursor.execute(query)
-                
-                # read in image
-                img_path, label = cursor.fetchone()
-                img_path = Path(self.data_dir, img_path)
-                image = ensure_rgb(np.array(Image.open(img_path))) # channels-last bc cv2
+        img = Image.open(self.dir / self.image_paths[idx])
+        label = self.labels[idx]
+        if self.transform:
+            img = self.transform(img)
         
-
-                
-                for l in self.label_dict.keys():
-                    if label == l:
-                        label = int(self.label_dict[l])
-                        break
-                if self.transform:
-                    image = self.transform(image)
-                if self.target_transform:
-                    label = self.target_transform(label)
-
-                if type(label) is not int:
-                    raise TypeError(f'Label must be an int, but got {type(label)}')
-        finally:
-            cursor.close()
-
-        return {'image': image, 'label': label}
-    
-    
+        label = self.label_dict[label] # convert label to int
+        return img, label
 
 def powerset_without_emptyset(items):
     '''
